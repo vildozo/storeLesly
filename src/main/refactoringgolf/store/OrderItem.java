64950df2b9526871527af2118a38594ae826578b
vildozo <vildozo@outlook.com>
2017-03-10 11:04:51 -0400

    EXTRACT METHOD for calculating discounts

--- src/main/refactoringgolf/store/OrderItem.java
+++ src/main/refactoringgolf/store/OrderItem.java
@@ -24,24 +24,38 @@ public class OrderItem {
 	float totalItem() {
 		float totalItem=0;
 		if (getProduct().getCategory() == ProductCategory.Accessories) {
-			float booksDiscount = 0;
-			if (itemAmount() >= 100) {
-				booksDiscount = itemAmount() * 10 / 100;
-			}
+			float booksDiscount = calculateAccessoriesDiscount();
 			totalItem = itemAmount() - booksDiscount;
 		}
 		if (getProduct().getCategory() == ProductCategory.Bikes) {
 			// 20% discount for Bikes
-			totalItem = itemAmount() - itemAmount() * 20 / 100;
+			totalItem = itemAmount() - calculateBikeDiscount();
 		}
 		if (getProduct().getCategory() == ProductCategory.Cloathing) {
-			float cloathingDiscount = 0;
-			if (getQuantity() > 2) {
-				cloathingDiscount = getProduct().getUnitPrice();
-			}
+			float cloathingDiscount = calculateCloathingDiscount();
 			totalItem = itemAmount() - cloathingDiscount;
 		}
 		return totalItem;
+	}
+
+	private float calculateCloathingDiscount() {
+		float cloathingDiscount = 0;
+		if (getQuantity() > 2) {
+			cloathingDiscount = getProduct().getUnitPrice();
+		}
+		return cloathingDiscount;
+	}
+
+	private float calculateBikeDiscount() {
+		return itemAmount() * 20 / 100;
+	}
+
+	private float calculateAccessoriesDiscount() {
+		float booksDiscount = 0;
+		if (itemAmount() >= 100) {
+			booksDiscount = itemAmount() * 10 / 100;
+		}
+		return booksDiscount;
 	}
 
 	private float itemAmount() {

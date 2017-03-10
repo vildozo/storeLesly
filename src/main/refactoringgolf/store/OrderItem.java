f4100a17f893eaa7367c67efec3d3b2a3a0c20f7
vildozo <vildozo@outlook.com>
2017-03-10 10:57:59 -0400

    MOVE totalitem in class Order to class OrderItem

--- src/main/refactoringgolf/store/OrderItem.java
+++ src/main/refactoringgolf/store/OrderItem.java
@@ -20,4 +20,28 @@ public class OrderItem {
 	public int getQuantity() {
 		return quantity;
 	}
+
+	float totalItem() {
+		float totalItem=0;
+		float itemAmount = getProduct().getUnitPrice() * getQuantity();
+		if (getProduct().getCategory() == ProductCategory.Accessories) {
+			float booksDiscount = 0;
+			if (itemAmount >= 100) {
+				booksDiscount = itemAmount * 10 / 100;
+			}
+			totalItem = itemAmount - booksDiscount;
+		}
+		if (getProduct().getCategory() == ProductCategory.Bikes) {
+			// 20% discount for Bikes
+			totalItem = itemAmount - itemAmount * 20 / 100;
+		}
+		if (getProduct().getCategory() == ProductCategory.Cloathing) {
+			float cloathingDiscount = 0;
+			if (getQuantity() > 2) {
+				cloathingDiscount = getProduct().getUnitPrice();
+			}
+			totalItem = itemAmount - cloathingDiscount;
+		}
+		return totalItem;
+	}
 }
